cd464fec429bbb8993c11951bb3345050cef3f57
Ali Servet Donmez <asd@pittle.org>
2021-09-09 15:09:30 +0200

    Showcase the bug

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,16 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+it("as expected, *does* log AND shows # calls", () => {
+  cy.intercept('https://example.cypress.io', cy.stub().log(true).as('foo'));
+
+  cy.visit('https://example.cypress.io');
+
+  cy.get('@foo').should('have.been.calledOnce');
+});
+
+it("as expected, does *not* log BUT doesn't shows # calls either", () => {
+  cy.intercept('https://example.cypress.io', cy.stub().log(false).as('foo'));
+
+  cy.visit('https://example.cypress.io');
+
+  cy.get('@foo').should('have.been.calledOnce');
+});
